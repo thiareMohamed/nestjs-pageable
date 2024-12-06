288d3228ff31eb29dc3bbe2c1c5ff371fb0d4f09
thiareMohamed <thiaremohamed.mt@gmail.com>
2024-12-06 13:06:13 +0000

    Mise à jour de la version du projet de 0.0.4 à 0.0.6 dans package.json et package-lock.json, ajout de l'index.ts pour l'exportation des modules et services

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * from './pagination.module';
+export * from './decorators/pageable.decorator';
+export * from './services/pagination.facade';
+export * from './dto/page.dto';
+export * from './dto/pageable.dto';
